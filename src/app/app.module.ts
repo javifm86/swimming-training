bdba481076e800cc6e1c511339132b9651ef4a23
javifm86 <javifm86@gmail.com>
2021-10-04 11:26:18 +0200

    Add auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { StoreModule } from '@ngrx/store';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AuthModule,
     StoreModule.forRoot({}, {})
   ],
   providers: [],
