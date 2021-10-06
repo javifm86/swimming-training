b0bb562dacb8eca9fc7da830f69de5942ae34f19
javifm86 <javifm86@gmail.com>
2021-10-06 16:18:00 +0200

    Add user to store if already in localStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { login } from './auth/auth.actions';
+import { AppState } from './reducers';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'swimming-training';
+export class AppComponent implements OnInit {
+  constructor(private router: Router, private store: Store<AppState>) {}
+
+  ngOnInit() {
+    const strUser = localStorage.getItem('user');
+
+    if (strUser) {
+      this.store.dispatch(login({ user: JSON.parse(strUser) }));
+    }
+  }
 }
