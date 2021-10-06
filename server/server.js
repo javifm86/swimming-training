e5ee9e7cb457d929f13e888098c127f2637f8c54
javifm86 <javifm86@gmail.com>
2021-10-06 11:31:18 +0200

    Add nodemon

--- server/server.js
+++ server/server.js
@@ -10,14 +10,24 @@ server.use(middlewares);
 server.use(pause(300));
 
 server.use((req, res, next) => {
-  console.log(req.headers);
+  // console.log(req.headers);
   next();
 });
-
+server.use(jsonServer.bodyParser);
 server.post("/api/login", (req, res) => {
-  res.status(401).json({
-    error: "unauthorized",
-  });
+  console.log(req.body);
+  if (req.body.user === "admin" || req.body.user === "javi") {
+    res.status(200).json({
+      user: req.body.user,
+      token:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIzNTM0NTQzNTQzNTQzNTM0NTMiLCJleHAiOjE1MDQ2OTkyNTZ9.zG-2FvGegujxoLWwIQfNB5IT46D-xC4e8dEDYwi6aRM",
+      permission: req.body.user === "admin" ? 10 : 5,
+    });
+  } else {
+    res.status(401).json({
+      error: "unauthorized",
+    });
+  }
 });
 
 server.use("/api", router);
