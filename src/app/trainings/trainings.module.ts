be9b5db9a0d15a1c4d68cc513853f2289654e0bc
javifm86 <javifm86@gmail.com>
2021-10-07 12:05:18 +0200

    Add exercises component

--- src/app/trainings/trainings.module.ts
+++ src/app/trainings/trainings.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ExercisesComponent } from './exercises/exercises.component';
 
 export const trainingRoutes: Routes = [
   {
     path: '',
-    component: HomeComponent,
+    component: HomeComponent
+  },
+  {
+    path: 'exercises',
+    component: ExercisesComponent
   }
 ];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    ExercisesComponent
+  ],
   imports: [
     CommonModule,
     RouterModule.forChild(trainingRoutes)
